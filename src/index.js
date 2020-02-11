d124e96df06a406e113b76e45ea2dec659fff18d
hchiam <hchiam@gmail.com>
2020-02-11 11:39:40 -0500

    first example + surge

--- src/index.js
+++ src/index.js
@@ -1,27 +1,49 @@
 /* eslint-disable require-jsdoc */
 
-const solution = (a, b) => {
-  return a + b;
+const config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: {y: 200},
+    },
+  },
+  scene: {
+    preload: preload,
+    create: create,
+  },
 };
 
-function solutionWrapper(...parameters) {
-  return solution(...parameters);
+const game = new Phaser.Game(config);
+
+function preload() {
+  this.load.setBaseURL('http://labs.phaser.io');
+
+  this.load.image('sky', 'assets/skies/space3.png');
+  this.load.image('logo', 'assets/sprites/phaser3-logo.png');
+  this.load.image('red', 'assets/particles/red.png');
 }
 
-// function ListNode(val) {
-//   this.val = val;
-//   this.next = null;
-// }
+function create() {
+  this.add.image(400, 300, 'sky');
 
-// function TreeNode(val) {
-//   this.val = val;
-//   this.left = this.right = null;
-// }
+  const particles = this.add.particles('red');
 
-if (typeof module !== 'undefined') {
-  module.exports = {
-    solutionWrapper,
-  };
+  const emitter = particles.createEmitter({
+    speed: 100,
+    scale: {start: 1, end: 0},
+    blendMode: 'ADD',
+  });
+
+  const logo = this.physics.add.image(400, 100, 'logo');
+
+  logo.setVelocity(100, 200);
+  logo.setBounce(1, 1);
+  logo.setCollideWorldBounds(true);
+
+  emitter.startFollow(logo);
 }
 
 /**
